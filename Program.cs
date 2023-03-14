babada4b9c27e6d428b807b11064ba722fb61683
traplord92 <traplord345@gmail.com>
2023-03-14 23:00:52 +0500

    Flow add

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿
-string[] masterArray = new string[5] {"I'm", " a Young Money ", "millionaire", " tougher", "than Nigerian hair"}; 
+string[] masterArray = new string[5] {"Angola", "31 ", "1992", " Russia", "Germany"}; 
 string[] theRuler_array = new string[masterArray.Length];
+
+void theCheker(string[] masterArray, string[] theRuler_array)
+{ 
+    int count = 0;
+    
+    for (int i = 0; i < masterArray.Length; i++)
+    {
+    if(masterArray[i].Length <= 3)
+        {
+        theRuler_array[count] = masterArray[i];
+        count++;
+        }
+    }
+}
